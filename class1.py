6373fc55f50d1cd41e0ea689f432971b1c2effe2
KeziaSJ <kezia@deerom.com>
2021-02-11 10:16:02 +0530

    Vehicle Modified

--- class1.py
+++ class1.py
@@ -1,7 +1,3 @@
 class Vehicle:
-    def __init__(self, max_speed, mileage):
-        self.max_speed = max_speed
-        self.mileage = mileage
+	pass
 
-modelX = Vehicle(240, 18)
-print(modelX.max_speed, modelX.mileage)
