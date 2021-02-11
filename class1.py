8045f4e66c2d1f9dc44a3e91ae3149741f65e2d0
shilpaalex <shilpa@deerom.com>
2021-02-11 10:45:35 +0530

    unmodified

--- class1.py
+++ class1.py
@@ -1,3 +1,9 @@
 class Vehicle:
-	pass
+
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+class Bus(Vehicle):
+    pass
 
