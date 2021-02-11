7b7aa07e0579d3e216459c42f43dcfd7076d6712
shilpaalex <shilpa@deerom.com>
2021-02-11 11:10:49 +0530

    modified

--- class1.py
+++ class1.py
@@ -1,11 +1,17 @@
 class Vehicle:
-
     def __init__(self, name, max_speed, mileage):
         self.name = name
         self.max_speed = max_speed
         self.mileage = mileage
+
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers"
+
 class Bus(Vehicle):
-    pass
+    def seating_capacity(self, capacity=50):
+        return super().seating_capacity(capacity=50)
 
 School_bus = Bus("School Volvo", 180, 12)
-print("Vehicle Name:", School_bus.name, "Speed:", School_bus.max_speed, "Mileage:", School_bus.mileage)
+     print(School_bus.seating_capacity())
+     print("modified")
+
