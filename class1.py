5b64e76617caa88047f84010bc0d00725efa5a7f
KeziaSJ <kezia@deerom.com>
2021-02-11 10:57:06 +0530

    Again Modified by adding object

--- class1.py
+++ class1.py
@@ -7,3 +7,5 @@ class Vehicle:
 class Bus(Vehicle):
     pass
 
+School_bus = Bus("School Volvo", 180, 12)
+print("Vehicle Name:", School_bus.name, "Speed:", School_bus.max_speed, "Mileage:", School_bus.mileage)
